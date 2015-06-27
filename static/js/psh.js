b709ea460e3c4d8322ef277d615ef182c9dc03ee
Martin W <martas@imm.cz>
2015-06-27 09:24:39 +0200

    License attribution fixes

--- static/js/psh.js
+++ static/js/psh.js
@@ -1,63 +1,72 @@
 // functions
-var messagePub = function(data){
-	// data.subtitle = data.subtitle && data.subtitle.length > 0 ? ' - '+data.subtitle: '';
 
-	fillMeta('.title', data.title );
-	fillMeta('.subtitle', data.subtitle );
-	fillMeta('.description', nl2br(data.description));
+var messagePub = function(data){
+    fillMeta('.title', data.title );
+    fillMeta('.subtitle', data.subtitle );
+    fillMeta('.description', nl2br(data.description));
     fillMeta('.attribution', mkattrib(data));
 
-	//console.log($('.description').height());
 
 };
 
 
 var fillMeta = function(f, t){
 	var msg = $('#messaging');
+
 	if(!t || t.length==0){
 		msg.find(f).html('');
-	}else{
+	} else {
 		msg.find(f).html(t);
 	}
 }
 
+
 var nl2br = function(str){
-	var breakTag = '<br>'; 
-	return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+    var breakTag = '\n</p><p>\n'; 
+    return  "<p>\n" + (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2') + "\n</p>";
 }
+
 
 
 var mkattrib = function(data){
     var author = "";
     var lic = "";
     if (data.url) {
-        author = "<a href='" + data.url +  "' target='_blank'>" + data.author + "</a>";
+        author = "<a href='" + data.url +  "'>" + data.author + "</a>";
     } else {
         author = data.author;
     }
 
-    console.log(data);
     var lic_deed;
 
     lic = data.rights;
     if (lic == "CC 0"){
         lic_deed = "https://creativecommons.org/publicdomain/zero/1.0/";
 
+    } else if (lic == "Public domain") {
+        return data.title + " je <a href='https://cs.wikipedia.org/wiki/Voln%C3%A9_d%C3%ADlo' target='_blank'>dílem volným</a>"
+
     } else if (lic.startsWith("CC BY") ) {
         x = lic.split(" ");
-        lic_deed = "https://creativecommons.org/licenses/" + x[1] + "/" + x[2] + "/";
-    }  
+        lic_deed = "https://creativecommons.org/licenses/" + x[1] + "/" + x[2] + "/cz/";
+
+    }  else {
+
+        return ""; // Proprietary license
+    }
     
 
     if (lic_deed) {
         lic = "<a href='" + lic_deed.toLowerCase() +  "' target='_blank'>" + data.rights + "</a>";
     }
 
-
-    var result = data.title + " by " + author + " is licensed under " + lic;
+    var result = data.title + " autora " + author + " pod licencí " + lic;
     return result
 
 }
+
+
+
 
 
 
@@ -68,6 +77,9 @@ var _statuschanged = function(state) {
    $("#footer").html("");
   }*/
 };
+
+
+
 
 
 var _connect = function(channel) {
