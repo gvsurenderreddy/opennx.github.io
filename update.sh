e5319e545ad4db4dc088537888d7bc01722002dd
Martastain <martas@imm.cz>
2016-03-07 11:16:44 +0100

    Added julia player updater

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp vendor/julia-player/dist/css/ionicons.min.css static/css/
+cp vendor/julia-player/dist/css/themes/default/julia.css static/css/themes/default/
+cp vendor/julia-player/dist/fonts/ionicons.eot static/fonts/
+cp vendor/julia-player/dist/fonts/ionicons.svg static/fonts/
+cp vendor/julia-player/dist/fonts/ionicons.ttf static/fonts/
+cp vendor/julia-player/dist/fonts/ionicons.woff static/fonts/
+cp vendor/julia-player/dist/js/julia-player.js static/js/
+cp vendor/julia-player/dist/swf/flashlsChromeless.swf static/swf/
+
